731b57c4dc5b0ff5c9e4f947dc08cab1b02b1711
Jared Lim <jorlim7@uw.edu>
2022-02-13 21:02:12 -0800

    Cleaning up project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 340game-finder
+# Project 2
+
+This repository contains code for an interactive web app, created for the _Client-Side Web Development_ course at the UW iSchool.
+
+The site can be viewed at <https://firebase-project-id.firebaseapp.com//>
--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBohq1cd_O1uonQy8E7448tWy6qmCnFGW0",
-  authDomain: "info-340-project-2-gamefinder.firebaseapp.com",
-  projectId: "info-340-project-2-gamefinder",
-  storageBucket: "info-340-project-2-gamefinder.appspot.com",
-  messagingSenderId: "126894449744",
-  appId: "1:126894449744:web:0fc717385b5315ff09dd55",
-  measurementId: "G-FXGRTMW1TC"
+  apiKey: "AIzaSyDy7rUvwrZVwMY4PF98E8r5Pbq0P23c-aA",
+  authDomain: "gamefinder-8cc38.firebaseapp.com",
+  projectId: "gamefinder-8cc38",
+  storageBucket: "gamefinder-8cc38.appspot.com",
+  messagingSenderId: "93596669578",
+  appId: "1:93596669578:web:e630497833b39cd12eff73",
+  measurementId: "G-0YL0T5BSTK"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
